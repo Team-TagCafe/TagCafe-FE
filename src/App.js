dc3a8d777bc230f79fc98a44665ec7ce45fe8f37
김민지 <ghi512151@gmail.com>
2025-01-02 20:27:55 +0100

    [feat] #2 - TextInput 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import LocationReset from "./components1/LocationReset";
 import VisitStatus from "./components1/VisitStatus";
@@ -9,92 +8,116 @@ import ShortButton from './components1/ShortButton';
 import ReviewStar from './components1/ReviewStar';
 import DetailButton from './components1/DetailButton';
 import SearchResult from './components1/SearchResult';
+import TextInput from './components1/TextInput';
+import { useState } from 'react';
 
 function App() {
-    // LocationReset 동작 함수
-    const handleLocationReset = () => {
-      console.log("위치가 초기화되었습니다.");
-    };
+  // LocationReset 동작 함수
+  const handleLocationReset = () => {
+    console.log("위치가 초기화되었습니다.");
+  };
 
-    // VisitStatus 변경 함수
-    const handleVisitStatusChange = (isVisited) => {
-      console.log(isVisited ? "방문 상태: 방문함" : "방문 상태: 방문하지 않음");
-    };
+  // VisitStatus 변경 함수
+  const handleVisitStatusChange = (isVisited) => {
+    console.log(isVisited ? "방문 상태: 방문함" : "방문 상태: 방문하지 않음");
+  };
 
-    // Bookmark 상태 변경 함수
-    const handleBookmarkChange = (isBookmarked) => {
-      if (isBookmarked) {
-        console.log("북마크가 설정되었습니다!");
-      } else {
-        console.log("북마크가 해제되었습니다!");
-      }
-    };
+  // Bookmark 상태 변경 함수
+  const handleBookmarkChange = (isBookmarked) => {
+    if (isBookmarked) {
+      console.log("북마크가 설정되었습니다!");
+    } else {
+      console.log("북마크가 해제되었습니다!");
+    }
+  };
 
-    // SideMenuButton 클릭 함수
-    const handleSideMenuClick = (optionText) => {
-      console.log(`사이드 메뉴에서 선택된 옵션: ${optionText}`);
-    };
+  // SideMenuButton 클릭 함수
+  const handleSideMenuClick = (optionText) => {
+    console.log(`사이드 메뉴에서 선택된 옵션: ${optionText}`);
+  };
 
-    // LongButton 클릭 함수
-    const handleLongButtonClick = (optionText) => {
-      console.log(`LongButton 클릭됨: ${optionText}`);
-    };
+  // LongButton 클릭 함수
+  const handleLongButtonClick = (optionText) => {
+    console.log(`LongButton 클릭됨: ${optionText}`);
+  };
 
-    // ShortButton 클릭 함수
-    const handleShortButtonClick = (optionText) => {
-      console.log(`ShortButton 클릭됨: ${optionText}`);
-    };
+  // ShortButton 클릭 함수
+  const handleShortButtonClick = (optionText) => {
+    console.log(`ShortButton 클릭됨: ${optionText}`);
+  };
 
-    // ReviewStar 클릭 함수
-    const handleReviewStarClick = (rating) => {
-      console.log(`선택된 별점: ${rating}점`);
-    };
+  // ReviewStar 클릭 함수
+  const handleReviewStarClick = (rating) => {
+    console.log(`선택된 별점: ${rating}점`);
+  };
 
-    // DetailButton 클릭 함수
-    const handleDetailButtonClick = () => {
-      console.log("카페 상세보기 버튼이 클릭되었습니다.");
-    };
+  // DetailButton 클릭 함수
+  const handleDetailButtonClick = () => {
+    console.log("카페 상세보기 버튼이 클릭되었습니다.");
+  };
 
-    // SearchResult 선택 함수
-    const handleSearchResultClick = (cafeName) => {
-      console.log(`선택된 카페: ${cafeName}`);
-    };
+  // SearchResult 선택 함수
+  const handleSearchResultClick = (cafeName) => {
+    console.log(`선택된 카페: ${cafeName}`);
+  };
 
-    return (
-      <div className="App">
-        {/* LocationReset 버튼 */}
-        <LocationReset onClick={handleLocationReset} />
+  // TextInput 함수
+  const [inputValue, setInputValue] = useState('');
 
-        {/* VisitStatus 버튼 */}
-        <VisitStatus onClick={handleVisitStatusChange} />
+  const handleInputChange = (value) => {
+    setInputValue(value);
+    console.log(value); // 입력된 내용 콘솔에 출력
+  };
 
-        {/* Bookmark 버튼 */}
-        <Bookmark onClick={handleBookmarkChange} />
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      console.log('Enter key pressed');
+    }
+  };
 
-        {/* SideMenuButton 버튼 */}
-        <SideMenuButton buttonType="button1" optionText="개인정보처리방침" onClick={handleSideMenuClick} />
-        <SideMenuButton buttonType="button2" optionText="이용 약관" onClick={handleSideMenuClick} />
+  return (
+    <div className="App">
+      {/* LocationReset 버튼 */}
+      <LocationReset onClick={handleLocationReset} />
 
-        {/* LongButton 버튼 */}
-        <LongButton optionText={"확인"} onClick={handleLongButtonClick} />
+      {/* VisitStatus 버튼 */}
+      <VisitStatus onClick={handleVisitStatusChange} />
 
-        {/* ShortButton 버튼 */}
-        <ShortButton optionText={"수정"} onClick={handleShortButtonClick} />
+      {/* Bookmark 버튼 */}
+      <Bookmark onClick={handleBookmarkChange} />
 
-        {/* ReviewStar 버튼 */}
-        <ReviewStar onClick={handleReviewStarClick} />
+      {/* SideMenuButton 버튼 */}
+      <SideMenuButton buttonType="button1" optionText="개인정보처리방침" onClick={handleSideMenuClick} />
+      <SideMenuButton buttonType="button2" optionText="이용 약관" onClick={handleSideMenuClick} />
 
-        {/* DetailButton 버튼 */}
-        <DetailButton optionText="카페 상세보기 >" onClick={handleDetailButtonClick} />
+      {/* LongButton 버튼 */}
+      <LongButton optionText={"확인"} onClick={handleLongButtonClick} />
 
-        {/* SearchResult 버튼 */}
-        <SearchResult 
-          cafeName="스테이 어도러블" 
-          address="경기 용인시 기흥구 죽전로43번길 15-3 (보정동)"
-          onClick={handleSearchResultClick}
-        />
-      </div>
-    );
+      {/* ShortButton 버튼 */}
+      <ShortButton optionText={"수정"} onClick={handleShortButtonClick} />
+
+      {/* ReviewStar 버튼 */}
+      <ReviewStar onClick={handleReviewStarClick} />
+
+      {/* DetailButton 버튼 */}
+      <DetailButton optionText="카페 상세보기 >" onClick={handleDetailButtonClick} />
+
+      {/* SearchResult 버튼 */}
+      <SearchResult
+        cafeName="스테이 어도러블"
+        address="경기 용인시 기흥구 죽전로43번길 15-3 (보정동)"
+        onClick={handleSearchResultClick}
+      />
+
+      {/* TextInput 버튼 */}
+      <TextInput
+        placeholder="input"
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+      />
+    </div>
+  );
 }
 
 export default App;
