d17d587300e128bbe0691fb6dd249c4850685d4c
김민지 <ghi512151@gmail.com>
2025-01-02 07:58:24 +0100

    [Feat] #2 - SideMenuButton 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import LocationReset from "./components1/LocationReset";
 import VisitStatus from "./components1/VisitStatus";
 import Bookmark from "./components1/Bookmark";
+import SideMenuButton from './components1/SideMenuButton';
 
 function App() {
   return (
@@ -15,6 +16,10 @@ function App() {
 
       {/* Bookmark 버튼 */}
       <Bookmark/>
+
+      {/* SideMenuButton 버튼 */}
+      <SideMenuButton buttonType="button1" optionText="개인정보처리방침" />
+      <SideMenuButton buttonType="button2" optionText="개인정보처리방침" />
 
     </div>
   );
