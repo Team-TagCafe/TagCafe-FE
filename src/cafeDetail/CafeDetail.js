0175fadaebf5d7c467733db7ccf203fd0a823c79
김민지 <ghi512151@gmail.com>
2025-01-21 18:46:00 +0100

    [feat] #17 - CafeDetail 구현

--- src/cafeDetail/CafeDetail.js
+++ src/cafeDetail/CafeDetail.js
@@ -1,15 +1,37 @@
 import React from "react";
 import { useParams } from "react-router-dom"
-import { BottomBar } from "../components";
+import {
+  BottomBar,
+  Bookmark,
+  CafeInformationDetail,
+} from "../components";
+import './CafeDetail.css';
+import ImageCarousel from "./ImageCarousel";
 
 const CafeDetail = () => {
-  const { id } = useParams();
+  const { id } = useParams(); // 카페 id
 
   return (
-    <div>
-      <h1>카페 상세 페이지</h1>
-      <p>카페 ID: {id}</p>
+    <div className="cafe-detail-page">
+      <div className="cafe-detail-image">
+        <ImageCarousel />
+      </div>
+
+      <div className="cafe-detail-header">
+        <div className="cafe-detail-header-text">
+          <div className="cafe-detail-name">스테이 어도러블</div>
+          <div className="cafe-detail-address">경기 용인시 기흥구 죽전로43번길 15-3 1층</div>
+        </div>
+        <Bookmark width="17px" height="31px" />
+      </div>
+
+      <div className="cafe-detail-divider"></div>
+      <div className="cafe-info-title">카페 정보</div>
+      <CafeInformationDetail />
+      <div className="cafe-detail-divider"></div>
+
       <BottomBar />
+
 
     </div>
   );
