840cacbfbcf32455848d7dea438b7732e6e9aad7
jinlee <dlwls6877@naver.com>
2025-04-14 14:13:23 +0900

    [fix] #39- CafeEdit 경로 수정

--- src/admin/CafeEdit.js
+++ src/admin/CafeEdit.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = `${API_BASE_URL}/cafes`;
+const API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}/cafes`;
 
 const CafeEdit = () => {
   const [cafes, setCafes] = useState([]);
